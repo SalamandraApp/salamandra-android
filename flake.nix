0f64515a15330c0c0fa3317d2287f39c55cdf23d
Jaimevzkz <jaimepva@ucm.es>
2024-06-23 12:17:37 +0200

    Clean working directory

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Salamandra App";
+  description = "Salamandra Server";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -16,13 +16,29 @@
           nativeBuildInputs = with pkgs; [
             awscli2
             nodePackages."@aws-amplify/cli"
+            pkgs.buildFHSUserEnv
           ];
 
           shellHook = ''
-            echo -e "Entering \e[1;31mSALAMANDRA-APP SHELL\e[0m"
+            echo -e "Entering \e[1;31mSALAMANDRA-SERVER SHELL\e[0m"
             echo -e " - AWS CLI"
             echo -e " - AWS Amplify CLI"
           '';
         };
+
+        apps = {
+          amplify = pkgs.buildFHSUserEnv {
+            name = "amplify-env";
+            targetPkgs = pkgs: [
+              pkgs.awscli2
+              pkgs.nodejs
+              pkgs.glibc
+              pkgs.libstdcxx
+              nodePackages."@aws-amplify/cli"
+            ];
+            runScript = "${nodePackages."@aws-amplify/cli"}/bin/amplify";
+          };
+        };
       });
 }
+
