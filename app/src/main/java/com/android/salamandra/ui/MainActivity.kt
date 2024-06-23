368589f78c6095838ad6648554ec1b323e668b76
Jaimevzkz <jaimepva@ucm.es>
2024-06-23 11:20:57 +0200

    update

--- app/src/main/java/com/android/salamandra/ui/MainActivity.kt
+++ app/src/main/java/com/android/salamandra/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.android.salamandra.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.kotlin.core.Amplify
 import com.android.salamandra.NavGraphs
 import com.android.salamandra.ui.theme.SalamandraTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
@@ -18,13 +22,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        try {
-//            Amplify.addPlugin(AWSCognitoAuthPlugin())
-//            Amplify.configure(applicationContext)
-//            Log.i("Jaime", "Initialized Amplify")
-//        } catch (error: AmplifyException) {
-//            Log.e("Jaime", "Could not initialize Amplify", error)
-//        }
+        try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.configure(applicationContext)
+            Log.i("Jaime", "Initialized Amplify")
+        } catch (error: AmplifyException) {
+            Log.e("Jaime", "Could not initialize Amplify", error)
+        }
 
         setContent {
             SalamandraTheme {
