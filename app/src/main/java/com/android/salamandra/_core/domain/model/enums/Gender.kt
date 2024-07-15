e5cdf69bf5cd077ac1e346fb3f542cb3a8395264
Jaimevzkz <jaimepva@ucm.es>
2024-07-15 11:43:13 +0200

    continue see workout flow.

--- app/src/main/java/com/android/salamandra/_core/domain/model/enums/Gender.kt
+++ app/src/main/java/com/android/salamandra/_core/domain/model/enums/Gender.kt
@@ -3,16 +3,12 @@ package com.android.salamandra._core.domain.model.enums
 enum class Gender {
     Male,
     Female,
-    Other,
-    Helicopter,
-    Grasshopper
+    Other
 }
 
 fun Int.toGender() = when(this){
     0 -> Gender.Male
     1 -> Gender.Female
     2 -> Gender.Other
-    3 -> Gender.Helicopter
-    4 -> Gender.Grasshopper
     else -> throw IllegalArgumentException("integer to Gender not mapped")
 }
