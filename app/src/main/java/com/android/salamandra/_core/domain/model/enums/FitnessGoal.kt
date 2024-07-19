08e1f00c54fb3e9d50b51a6a08044e13d1fb5996
Jaimevzkz <jaimepva@ucm.es>
2024-07-19 14:44:01 +0200

    Add enums and finish alpha 0.2.0 sprint 1.

--- app/src/main/java/com/android/salamandra/_core/domain/model/enums/FitnessGoal.kt
+++ app/src/main/java/com/android/salamandra/_core/domain/model/enums/FitnessGoal.kt
@@ -1,10 +1,22 @@
 package com.android.salamandra._core.domain.model.enums
 
 enum class FitnessGoal {
-    Bulking
+    LoseWeight,
+    GainWeight,
+    BuildStrength,
+    BuildMuscleEndurance,
+    StayInShape,
+    Bulking,
+    Cut
 }
 
-fun Int.toFitnessGoal() = when(this){
-    0 -> FitnessGoal.Bulking
+fun Int.toFitnessGoal() = when (this) {
+    0 -> FitnessGoal.LoseWeight
+    1 -> FitnessGoal.GainWeight
+    2 -> FitnessGoal.BuildStrength
+    3 -> FitnessGoal.BuildMuscleEndurance
+    4 -> FitnessGoal.StayInShape
+    5 -> FitnessGoal.Bulking
+    6 -> FitnessGoal.Cut
     else -> throw IllegalArgumentException("Integer to FitnessGoal not mapped")
 }
--- app/src/main/java/com/android/salamandra/_core/domain/model/enums/FitnessLevel.kt
+++ app/src/main/java/com/android/salamandra/_core/domain/model/enums/FitnessLevel.kt
@@ -1,10 +1,22 @@
 package com.android.salamandra._core.domain.model.enums
 
 enum class FitnessLevel {
-    Beginner
+    Injured,
+    TakingABreak,
+    GettingStarted,
+    SomeExperience,
+    Amateur,
+    Athlete,
+    Elite
 }
 
-fun Int.toFitnessLevel() = when(this){
-    0 -> FitnessLevel.Beginner
+fun Int.toFitnessLevel() = when (this) {
+    0 -> FitnessLevel.Injured
+    1 -> FitnessLevel.TakingABreak
+    2 -> FitnessLevel.GettingStarted
+    3 -> FitnessLevel.SomeExperience
+    4 -> FitnessLevel.Amateur
+    5 -> FitnessLevel.Athlete
+    6 -> FitnessLevel.Elite
     else -> throw IllegalArgumentException("Integer to FitnessLevel not mapped")
 }
