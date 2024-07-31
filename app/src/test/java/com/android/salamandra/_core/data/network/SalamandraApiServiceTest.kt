32878730ca6d73345607758515cbf219976f9dd8
Jaimevzkz <jaimepva@ucm.es>
2024-07-31 17:59:44 +0200

    Start testing retrofit

--- app/src/test/java/com/android/salamandra/_core/data/network/SalamandraApiServiceTest.kt
+++ app/src/test/java/com/android/salamandra/_core/data/network/SalamandraApiServiceTest.kt
@@ -1,4 +1,50 @@
 package com.android.salamandra._core.data.network
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class SalamandraApiServiceTest {
+    private lateinit var mockWebServer: MockWebServer
+    private lateinit var salamandraApiService: SalamandraApiService
+
+    @Before
+    fun createServer() {
+        mockWebServer = MockWebServer()
+
+        salamandraApiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/")) // setting a dummy url
+            .client(OkHttpClient.Builder().build())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SalamandraApiService::class.java)
+    }
+
+    @After
+    fun shutdownServer() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun `When searchExercise is successful, ExerciseResponse parsed correctly and toDomain works`() = runTest {
+        // Arrange
+        val response = MockResponse().setBody(jsonSuccessfulSearchExerciseResponse)
+        mockWebServer.enqueue(response)
+        // Act
+        val result = salamandraApiService.searchExercise("")
+
+        // Assert
+        assert(result == parsedExerciseResponse && result.toDomain() == toDomainExerciseResponse)
+    }
+
+
 }
--- app/src/test/java/com/android/salamandra/_core/data/network/Util.kt
+++ app/src/test/java/com/android/salamandra/_core/data/network/Util.kt
@@ -1,4 +1,44 @@
 package com.android.salamandra._core.data.network
 
-class Util {
-}
+import com.android.salamandra._core.data.network.response.ExerciseResponse
+import com.android.salamandra._core.data.network.response.SingleExercise
+import com.android.salamandra._core.domain.model.Exercise
+import com.android.salamandra._core.domain.model.enums.Equipment
+import com.android.salamandra._core.domain.model.enums.ExerciseType
+import com.android.salamandra._core.domain.model.enums.MuscleGroup
+
+val jsonSuccessfulSearchExerciseResponse = """
+    {
+      "exercises": [
+        {
+          "id": "4495fa60-f5c6-4ec5-9942-203acf751822",
+          "name": "Push Up",
+          "main_muscle_group": 1,
+          "secondary_muscle_group": 15,
+          "necessary_equipment": 1,
+          "exercise_type": 1
+        }
+      ]
+    }
+""".trimIndent()
+
+val parsedExerciseResponse = ExerciseResponse(
+    items = listOf(SingleExercise(
+        id = "4495fa60-f5c6-4ec5-9942-203acf751822",
+        name = "Push Up",
+        mainMuscleGroup = 1,
+        secondaryMuscleGroup = 15,
+        necessaryEquipment = 1,
+        exerciseType = 1
+    ))
+)
+
+val toDomainExerciseResponse = listOf(Exercise(
+    exId = "4495fa60-f5c6-4ec5-9942-203acf751822",
+    name = "Push Up",
+    mainMuscleGroup = MuscleGroup.LowerChest,
+    secondaryMuscleGroup = MuscleGroup.Quadriceps,
+    necessaryEquipment = Equipment.Barbell,
+    exerciseType = ExerciseType.FreeWeights
+))
+
