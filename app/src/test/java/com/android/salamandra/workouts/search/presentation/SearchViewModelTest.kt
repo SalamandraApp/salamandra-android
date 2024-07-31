ac56cb3a016527e0b63ef98b8bd0aea0729b50e4
Jaimevzkz <jaimepva@ucm.es>
2024-07-31 17:14:10 +0200

    add test for search viewmodel

--- app/src/test/java/com/android/salamandra/workouts/search/presentation/SearchViewModelTest.kt
+++ app/src/test/java/com/android/salamandra/workouts/search/presentation/SearchViewModelTest.kt
@@ -1,5 +1,58 @@
 package com.android.salamandra.workouts.search.presentation
 
-import org.junit.jupiter.api.Assertions.*
+import com.android.salamandra.util.CoroutineRule
+import com.android.salamandra.workouts.search.domain.Repository
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-class SearchViewModelTest
+@OptIn(ExperimentalCoroutinesApi::class)
+class SearchViewModelTest {
+    private val testDispatcher = StandardTestDispatcher()
+
+    @get:Rule
+    val coroutineRule = CoroutineRule(testDispatcher)
+
+    private lateinit var searchViewModel: SearchViewModel
+
+    @RelaxedMockK
+    private lateinit var repository: Repository
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+
+        searchViewModel = SearchViewModel(testDispatcher, repository)
+    }
+
+    @Test
+    fun `Assert initial state`() {
+        val expectedState = SearchState(
+            error = null,
+            searchTerm = "",
+            searchResultExercises = emptyList(),
+            addedExercisesIds = emptyList(),
+            selectedExercise = null
+        )
+        assert(SearchState.initial == expectedState)
+    }
+
+    @Test
+    fun `Assert searchTerm changes correctly`() = runTest {
+        // Arrange
+        val expectedValue = "new search term"
+
+        // Act
+        searchViewModel.dispatch(SearchIntent.ChangeSearchTerm(expectedValue))
+        runCurrent()
+
+        // Assert
+        assert(searchViewModel.state.value.searchTerm == expectedValue)
+    }
+}
